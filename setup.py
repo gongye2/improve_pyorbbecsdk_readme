aeaf0c35f9c82778f18f23184f802f4baec4182f
gongye <chen22sdd@gmail.com>
2026-02-25 16:23:56 +0800

    Adjust setup.py to adapt to uv_cli

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 # ******************************************************************************
 #  Copyright (c) 2024 Orbbec 3D Technology, Inc
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http:# www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 # ******************************************************************************
 
 import os
@@ -45,13 +57,6 @@ class CustomBuildExt(build_ext):
                 shutil.copy2(source_path, destination_path)
 
 setup(
-    name='pyorbbecsdk2',
-    version='2.0.18',
-    author='zhonghong',
-    author_email='zhonghong@orbbec.com',
-    description='pyorbbecsdk is a python wrapper for the OrbbecSDK',
-    long_description=long_description,           
-    long_description_content_type="text/markdown",
     ext_modules=[PrebuiltExtension('pyorbbecsdk', 'install/lib')],
     cmdclass={'build_ext': CustomBuildExt},
     zip_safe=False,
